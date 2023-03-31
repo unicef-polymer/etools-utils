660f539f105325dd5a0c79394aae3258e7cf6d48
Adriana Trif <trif.corina.adriana@gmail.com>
2023-03-31 15:58:57 +0300

    navigation.util tweak

--- src_ts/navigation.util.ts
+++ src_ts/navigation.util.ts
@@ -1,7 +1,7 @@
 /**
  * 'popstate' event is usually listened to by the routing system of the app
  */
-export function pushState(path: string, eventToTrigger: string) {
+export function updateAppLocation(path: string, eventToTrigger: string) {
   history.pushState(window.history.state, '', path);  
   window.dispatchEvent(new CustomEvent(eventToTrigger ? eventToTrigger : 'popstate'));
 }
@@ -10,7 +10,7 @@ export function pushState(path: string, eventToTrigger: string) {
  * Use when you do not want to create another entry in the browser history.
  * So that the back button doesn't take you through another history entry.
  */
-export function replaceState(path: string, eventToTrigger: string) {
+export function replaceAppLocation(path: string, eventToTrigger: string) {
   history.replaceState(window.history.state, '', path);
   window.dispatchEvent(new CustomEvent(eventToTrigger ? eventToTrigger : 'popstate'));
 }
