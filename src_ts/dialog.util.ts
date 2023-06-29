d0900e0867a110b8c9bc4ec320d4aa6438229b3d
Trif Adriana <trif.corina.adriana@gmail.com>
2023-06-29 17:32:21 +0300

    Ajustment for sl-dialog

--- src_ts/dialog.util.ts
+++ src_ts/dialog.util.ts
@@ -3,7 +3,7 @@ import {IDialog, IDialogResponse} from './types/dialog.types';
 // you need to fire 'dialog-closed' event on dialog close!
 export function openDialog<D, R = any>({dialog, dialogData, readonly}: IDialog<D>): Promise<IDialogResponse<R>> {
   return new Promise((resolve: (detail: IDialogResponse<R>) => any, reject: (e: Error) => any) => {
-    const dialogElement: HTMLElement & IDialog<D> = document.createElement(dialog) as HTMLElement & IDialog<D>;
+    const dialogElement: HTMLElement & IDialog<D> & any = document.createElement(dialog) as HTMLElement & IDialog<D>;
     const body: HTMLBodyElement | null = document.querySelector('body');
     if (body) {
       body.appendChild(dialogElement);
@@ -11,17 +11,23 @@ export function openDialog<D, R = any>({dialog, dialogData, readonly}: IDialog<D
       reject(new Error('Body not exist'));
     }
     dialogElement.dialogData = dialogData;
+    let etoolsDialog: any;
     dialogElement.updateComplete.then(() =>
-      setTimeout(() => (dialogElement.shadowRoot!.querySelector<any>('etools-dialog')!.opened = true))
+      setTimeout(() => {
+        etoolsDialog = dialogElement.shadowRoot.querySelector('etools-dialog');
+        etoolsDialog.opened = true;
+      })
     );
+
     if (readonly) {
       dialogElement.readonly = readonly;
     }
     dialogElement.addEventListener('dialog-closed', (e: Event) => {
       const event: CustomEvent<IDialogResponse<R>> = e as CustomEvent<IDialogResponse<R>>;
       resolve(event.detail);
+      etoolsDialog.opened = false;
     });
-    dialogElement.addEventListener('sl-after-hide', (e) => {
+    dialogElement.addEventListener('sl-after-hide', (_e: Event) => {
       dialogElement.remove();
     });
   });
